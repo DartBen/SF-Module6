ddff1a345ce24d7c360518dbf3e7c21377e26b07
Dmitry <ryabchikk@rambler.ru>
2022-10-06 20:33:11 +0300

    6.4

---
+++ SF Module 6/6.3.1/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var department = GetCurrentDepartment();
+
+
+        string tmpStr = department.Company.Name;
+
+        if (department?.Company?.Type == "Банк" && department?.City?.Name  == "Санкт-Петербург")
+        {
+            Console.WriteLine($"У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+        }
+
+    }
+
+    static Department GetCurrentDepartment()
+    {
+        // logic
+
+        Department d = new Department();
+        return d;
+    }
+}
+
+class Company
+{
+    public string Type;
+    public string Name;
+}
+
+class Department
+{
+    public Company Company;
+    public City City;
+}
+
+class City
+{
+    public string Name;
+}
