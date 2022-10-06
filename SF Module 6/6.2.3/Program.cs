33d506de71a8e75a0a41b057524ceb3b392aab0f
Dmitry <ryabchikk@rambler.ru>
2022-10-06 19:44:59 +0300

    6.2.8

--- SF Module 6/6.2.3/Program.cs
+++ SF Module 6/6.2.3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Animal animal = new Animal { type = "Собака", name = "Вольт", age = 4 };
+    }
+}
+
+struct Animal
+{
+    // Поля структуры
+    public string type;
+    public string name;
+    public int age;
+
+    // Метод структуры
+    public void Info()
+    {
+        Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+    }
+}
---
+++ SF Module 6/6.2.8/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._8
+{
+    internal class Rectangle
+    {
+        int a, b;
+
+        public int Square() { return a * b; }
+
+        public Rectangle(int x)
+        {
+            a = x;
+            b = x;
+        }
+
+        public Rectangle(int x, int z)
+        {
+            a = x;
+            b = z;
+        }
+
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+
+    }
+}
