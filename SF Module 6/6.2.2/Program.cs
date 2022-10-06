069858d8668d485f192dcc531b96adee27c2dbb3
Dmitry <ryabchikk@rambler.ru>
2022-10-06 18:01:00 +0300

    6.2.2

---
+++ SF Module 6/6.2.2/Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Pen
+{
+    public string color;
+    public int cost;
+
+    public Pen()
+    {
+        color = "Черный";
+        cost = 100;
+    }
+
+    public Pen(string penColor, int penCost)
+    {
+        color = penColor;
+        cost = penCost;
+    }
+}
+
+class Human
+{
+    // Поля класса
+    public string name;
+    public int age;
+
+    // Метод класса
+    public void Greetings()
+    {
+        Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+    }
+
+    // Конструктор 1
+    public Human()
+    {
+        name = "Неизвестно";
+        age = 20;
+    }
+    // Конструктор 2
+    public Human(string n)
+    {
+        name = n;
+        age = 20;
+    }
+    // Конструктор 3
+    public Human(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+}
